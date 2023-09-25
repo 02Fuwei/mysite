1214dcf13c7bef405cc5cf668f03799a3d3237ea
02Fuwei <140091212+02Fuwei@users.noreply.github.com>
2023-09-25 11:17:56 +0800

    为 URL 名称添加命名空间

--- polls/urls.py
+++ polls/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path 
 from . import views
 
+app_name = "polls"
 urlpatterns = [
     path("",views.index,name="index"),
+    path("<int:quetsion_id>/",views.detail,name="detauk"),
     path("<int:question_id>/results/",views.results,name="results"),
     path("<int:question_id>/vote/",views.vote,name="vote"),
-    path("specifics/<int:quetsion_id>/",views.detail,name="detauk"),
 ]
