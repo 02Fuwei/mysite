eff38a1d2b3b060c68cb7e52b50de82bd62b7946
02Fuwei <140091212+02Fuwei@users.noreply.github.com>
2023-09-26 15:56:03 +0800

    1.自定义后台表单 2.添加关联的对象

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Question,Choice
 # Register your models here.
-admin.site.register(Question)
-admin.site.register(Choice)
+
+class ChoiceInIine(admin.TabularInline):
+    model = Choice
+    extra = 3
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,{"fields":["question_text"]}),
+        ("Date information",{"fields":["pub_date"]})
+    ]
+    inlines = [ChoiceInIine]
+admin.site.register(Question,QuestionAdmin)
