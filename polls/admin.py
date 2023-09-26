86eb455acbbbcda1f3e58a00b97caed084a49f3f
02Fuwei <140091212+02Fuwei@users.noreply.github.com>
2023-09-26 16:23:13 +0800

    1.自定义后台更改列表

--- polls/admin.py
+++ polls/admin.py
@@ -8,7 +8,10 @@ class ChoiceInIine(admin.TabularInline):
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,{"fields":["question_text"]}),
-        ("Date information",{"fields":["pub_date"]})
+        ("Date information",{"fields":["pub_date"],"classes":["collapse"]})
     ]
     inlines = [ChoiceInIine]
+    list_display = ["question_text","pub_date","was_published_recently"] # 排序
+    list_filter = ["pub_date"] #侧边栏 过滤器
+    search_fields = ["question_text"]  #搜索框
 admin.site.register(Question,QuestionAdmin)
