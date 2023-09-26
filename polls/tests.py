0f50907fd9d0e55e62f617b1b545f65d6183d278
02Fuwei <140091212+02Fuwei@users.noreply.github.com>
2023-09-26 11:42:16 +0800

    1.测试 DetailView 2.修改 IndexView中test中bug

--- polls/tests.py
+++ polls/tests.py
@@ -44,12 +44,26 @@ class QuestionIndexViewTest(TestCase):
         self.assertQuerysetEqual(response.context["latest_question_list"],[])
 
     def test_future_question_and_past_question(self):
-        question = create_question(question_text="Past question.",days=-30),create_question(question_text="Future question.",days=30)
+        question = create_question(question_text="Past question.",days=-30)
+        create_question(question_text="Future question.",days=30)
         response = self.client.get(reverse("polls:index"))
-        self.assertQuerysetEqual(response.context["latest_question_list"],[question])
+        self.assertQuerysetEqual(response.context["latest_question_list"],[question],)
     
     def test_two_past_questions(self):
         question1 = create_question(question_text="Past question1.",days=-30)
         question2 = create_question(question_text="Past question2.",days=-5)
         response = self.client.get(reverse("polls:index"))
-        self.assertQuerysetEqual(response.context["latest_question_list"],[question1,question2])
+        self.assertQuerysetEqual(response.context["latest_question_list"],[question2,question1],)
+
+class QuestionDetailViewTest(TestCase):
+    def test_futurn_question(self):
+        future_question = create_question(question_text="Future_question",days=5)
+        url = reverse("polls:detail",args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code,404)
+
+    def test_past_question(self):
+        past_question = create_question(question_text="Past_question",days=-5)
+        url = reverse("polls:detail",args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response,past_question.question_text)
--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,5 @@
+from typing import Any
+from django.db import models
 from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse,HttpResponseRedirect
 from .models import Question,Choice
@@ -19,6 +21,9 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
+    
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte = timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
